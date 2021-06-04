e752b24cf61b1dfbc40367f593570502657c1452
Francisco Heronildo <heronildo.2010@hotmail.com>
2021-06-04 15:21:13 -0300

    add somativas code

---
+++ Code/Somativas/1/divisao_de_inteiros.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+	n = 0;
+	d = 0;
+
+	while(a>b){
+		left_shift b;
+	}
+	while(n>0){
+		left_shift d;
+		right_shift b;
+		if(a>=b){
+			a<=a-b;
+			d<=d+1;
+		}
+	n<=n-1;
+	}
+
+	return 0;
+}
